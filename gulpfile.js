2be5719fcf62bfa7963acc387d0b05a724044a2f
Tharon Carlson <design@tharoncarlson.com>
2014-07-21 09:35:04 -0700

    simplified gulp file made changes to css and learning.js for practice. I know I should have branched but now you can see what Ive been doing

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-var gulp = require('gulp');
-var prefix = require('gulp-autoprefixer');
-var sass = require('gulp-sass');
-var refresh = require('gulp-livereload');
-var lr = require('tiny-lr'); 
-var server = lr();
+var gulp = require('gulp'),
+    prefix = require('gulp-autoprefixer'),
+    sass = require('gulp-sass'),
+    refresh = require('gulp-livereload'),
+    lr = require('tiny-lr'), 
+      server = lr();
 
 gulp.task('style', function() {
     return gulp.src('./sass/*.scss')
@@ -11,17 +11,6 @@ gulp.task('style', function() {
         .pipe(prefix('last 2 versions' , 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ))
         .pipe(gulp.dest('./css/'))
         .pipe(refresh(server));
-});
-
-gulp.task('livereload', function(){  
-    server.listen(35729, function(err){
-        if(err) return console.log(err);
-    });
-});
-
-gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch ('scss/*.scss').on('change', livereload.changed);
 });
 
 // default gulp watch task 
--- js/learning.js
+++ js/learning.js
@@ -1,4 +1,29 @@
 jQuery(document).ready(function($) {
-  $('.block').toggleClass('button-1');
-});
+  
+  $('dd')/*.filter(':nth-child(n+4)')*/.addClass('hide');
+  
+  $('dl').on('mouseenter','dt', function() {
+  	$(this)
+  	.next()
+  		 .slideDown(200)
+  			.siblings('dd')
+  				.slideUp(200);
+ 	});
+
+  	$('heading.title').delegate('h1', 'click', function() {
+  		console.log('clicked');
+  		$(this).clone().appendTo('.title');
+  	});
+
+  var co = $('span.co').each(function() {
+  	var $this = $(this);
+
+  	$('<blockquote></blockquote>', {
+  		class: 'quote',
+  		text: $this.text()
+  	}).prependTo( $this.closest('p') );
+  	
+  });
+});	
+
 
